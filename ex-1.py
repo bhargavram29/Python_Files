d73a0614c10818bfc322fb3ac2ea5bd72ad536c0
bhargavram29 <bhargavrampallapothu@gmail.com>
2022-07-29 19:43:19 +0530

    Modified ex-1

--- ex-1.py
+++ ex-1.py
@@ -1,8 +1,7 @@
-x = int(input("enter your age"))
+x = int(input("enter your age:- "))
 
 if x>=18 :
     print("you are eligible to vote")
 
 else :
-    print("ypu are not eligible to vote")
-    
+    print("you are not eligible to vote")
